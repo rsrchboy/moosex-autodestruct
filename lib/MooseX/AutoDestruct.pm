8a37194f16ce4a366576f22132e8e8043d6b64cb
Chris Weyl <cweyl@alumni.drew.edu>
2011-11-06 14:14:04 -0800

    update/shuffle POD

--- lib/MooseX/AutoDestruct.pm
+++ lib/MooseX/AutoDestruct.pm
@@ -36,47 +36,6 @@ Moose::Exporter->setup_import_methods(
 # debugging
 #use Smart::Comments '###', '####';
 
-=head1 SYNOPSIS
-
-    package Foo;
-
-    use Moose;
-    use namespace::autoclean;
-    use MooseX::AutoDestruct;
-
-    has foo => (
-        traits => ['AutoDestruct'],
-        is => 'ro', isa => 'Str', lazy_build => 1,
-        ttl => 600, # time, in seconds
-    );
-
-    sub _build_foo { --some expensive operation-- }
-
-=head1 DESCRIPTION
-
-MooseX::AutoDestruct is an attribute metaclass trait that causes your
-attribute value to be cleared after a certain time from when the value has
-been set.
-
-This trait will work regardless of how the value is populated or if a clearer
-method has been installed; or if the value is accessed via the installed
-accessors or by accessing the attribute metaclass itself.
-
-=head1 TRAITS APPLIED
-
-No traits are automatically applied to any metaclasses; however, on use'ing
-this package an 'AutoDestruct' attribute trait becomes available.
-
-=head1 USAGE
-
-Apply the AutoDestruct trait to your attribute metaclass (e.g. "traits =>
-['AutoDestruct']") and supply a ttl value.
-
-Typical usage of this could be for an attribute to store a value that is
-expensive to calculate, and can be counted on to be valid for a certain amount
-of time (e.g. caching).  Builders are your friends :)
-
-=cut
 
 {
     package Moose::Meta::Attribute::Custom::Trait::AutoDestruct;
@@ -293,13 +252,64 @@ of time (e.g. caching).  Builders are your friends :)
 
 }
 
+!!42;
+
+__END__
+
+=head1 SYNOPSIS
+
+    package Foo;
+
+    use Moose;
+    use namespace::autoclean;
+    use MooseX::AutoDestruct;
+
+    has foo => (
+        traits     => [ AutoDestruct ],
+        is         => 'ro',
+        isa        => 'Str',
+        lazy_build => 1,
+        ttl        => 600, # time, in seconds
+    );
+
+    sub _build_foo { --some expensive operation-- }
+
+=head1 DESCRIPTION
+
+MooseX::AutoDestruct is an attribute metaclass trait that causes your
+attribute value to be cleared after a certain time from when the value has
+been set.
+
+This trait will work regardless of how the value is populated or if a clearer
+method has been installed; or if the value is accessed via the installed
+accessors or by accessing the attribute metaclass itself.
+
+=head1 TRAITS APPLIED
+
+No traits are automatically applied to any metaclasses; however, on use'ing
+this package an 'AutoDestruct' attribute trait becomes available.
+
+Moose will properly deduce what trait you're talking about if you pass
+AutoDestruct as a string -- e.g.:
+
+    has foo => (traits => [ 'AutoDestruct' ], ...)
+
+However, this is depreciated in favor of the exported trait alias:
+
+    has foo => (traits => [ AutoDestruct ], ...)
+
+=head1 USAGE
+
+Apply the AutoDestruct trait to your attribute metaclass (e.g. "traits =>
+[AutoDestruct]") and supply a ttl value.
+
+Typical usage of this could be for an attribute to store a value that is
+expensive to calculate, and can be counted on to be valid for a certain amount
+of time (e.g. caching).  Builders are your friends :)
+
 =head1 SEE ALSO
 
-L<Class::MOP>, L<Moose>.
-
-=head1 AUTHOR
-
-Chris Weyl, C<< <cweyl at alumni.drew.edu> >>
+L<Moose>.
 
 =head1 BUGS
 
@@ -309,5 +319,3 @@ the web interface at
 L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-AutoDestruct>.
 
 =cut
-
-1; # End of MooseX::AutoDestruct
